a6baa4c1b9f0f7d609666f880a84b5a86083ec42
Ricardo <ricarditti5@gmail.com>
2026-02-13 15:47:12 +0000

    .

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $result = $conn->query("SELECT * FROM artigo ORDER BY id_artigo DESC");
 <html lang="pt">
 <head>
   <meta charset="UTF-8">
-  <title>Artigos</title>
+  <title>Martial Arts Shop</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="container mt-4 bg-black text-white">
